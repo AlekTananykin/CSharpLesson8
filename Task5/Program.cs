0a7a0246ece93f8021e7c5110aa0ec5553723889
AlekTananykin <tananykin@mail.ru>
2020-11-22 09:27:52 +0300

    Tesk5 initiate

---
+++ Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Тананыкин
+ * *Написать программу-преобразователь из CSV в XML-файл с 
+ *  информацией о студентах (6 урок).
+ */
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
